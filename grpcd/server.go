5173c3a51771cc990674175030d6fdf3de95e6cf
Rodrigo Fuenzalida <rfuenzalidac87@gmail.com>
2016-06-15 12:04:33 -0300

    remove all related with traces

--- grpcd/server.go
+++ grpcd/server.go
@@ -2,16 +2,10 @@ package grpcd
 
 import (
 	"errors"
-	"fmt"
 	"time"
 
-	kitot "github.com/go-kit/kit/tracing/opentracing"
-	"github.com/go-kit/kit/transport/grpc"
-	opentracing "github.com/opentracing/opentracing-go"
 	"github.com/rodrwan/traces/ptypes"
 	"golang.org/x/net/context"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/metadata"
 )
 
 // ErrBadMetadata ...
@@ -19,54 +13,33 @@ import (
 const (
 	ErrBadMetadata = "Bad metadata format"
 	ErrWrongSpan   = "Cannot create span"
+	ErrBadUserID   = "Wrong user ID"
 )
 
 // TestServer implements ptypes.CallbackServiceServer interface
 type TestServer struct {
-	Msg    string
-	Tracer opentracing.Tracer
+	Msg string
 }
 
 // NewTestServer constructs a new CallbackServer
-func NewTestServer(tracer opentracing.Tracer) (*TestServer, error) {
+func NewTestServer() (*TestServer, error) {
 	return &TestServer{
-		Msg:    "Init message",
-		Tracer: tracer,
+		Msg: "Init message",
 	}, nil
 }
 
 // Show returns endpoint for a given user_id and task_id.
 func (s *TestServer) Show(ctx context.Context, req *ptypes.TestRequest,
 ) (*ptypes.TestReply, error) {
-	md, ok := metadata.FromContext(ctx)
-	if !ok {
-		return nil, errors.New(ErrBadMetadata)
-	}
-
-	var fromGRPCFunc grpc.RequestFunc = kitot.FromGRPCRequest(
-		s.Tracer,
-		"Tracer /show",
-		nil,
-	)
-	fmt.Printf("%+v\n\n\n", ctx)
-
-	joinCtx := fromGRPCFunc(ctx, &md)
-	span := opentracing.SpanFromContext(joinCtx)
-	defer span.Finish()
-
-	span.SetTag("GRPC.Method", "Show")
-
 	userID := req.User
 
 	if userID == "" {
-		span.SetTag("GRPC.Codes", codes.InvalidArgument)
-		span.LogEvent("UserID cannot be nil")
+		return nil, errors.New(ErrBadUserID)
 	}
 
 	// Fake process
 	time.Sleep(200 * time.Millisecond)
 
-	span.SetTag("GRPC.Codes", codes.OK)
 	return &ptypes.TestReply{
 		Msg: "Hello " + userID,
 	}, nil
